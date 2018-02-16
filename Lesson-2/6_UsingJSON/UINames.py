850d71bbd3a81b35f8b3bee3308fa5924a5c6dc7
Josh Breckenridge <bloo1987@gmail.com>
2018-02-15 19:09:30 -0500

    Completed the lesson

--- Lesson-2/6_UsingJSON/UINames.py
+++ Lesson-2/6_UsingJSON/UINames.py
@@ -14,11 +14,14 @@ import requests
 def SampleRecord():
     r = requests.get("http://uinames.com/api?ext&region=United%20States",
                      timeout=2.0)
-    # 1. Add a line of code here to decode JSON from the response.
+
+    r_json = r.json()
 
     return "My name is {} {} and the PIN on my card is {}.".format(
-        # 2. Add the correct fields from the JSON data structure.
-    )
+        r_json['name'],
+        r_json['surname'],
+        r_json['credit_card']['pin'])
+
 
 if __name__ == '__main__':
     print(SampleRecord())
--- Lesson-2/7_BookmarkServer/BookmarkServer.py
+++ Lesson-2/7_BookmarkServer/BookmarkServer.py
@@ -74,8 +74,18 @@ def CheckURI(uri, timeout=5):
     False if that GET request returns any other response, or doesn't return
     (i.e. times out).
     '''
-    # 1. Write this function.  Delete the following line.
-    raise NotImplementedError("Step 1 isn't written yet.")
+    # Create request with 'uri'
+    try:
+        r = requests.get(uri, timeout=timeout)
+
+    except Exception:
+        return False
+
+    else:
+        # Check for 200 status code
+        if r.status_code == 200:
+            return True
+        return False
 
 
 class Shortener(http.server.BaseHTTPRequestHandler):
@@ -88,7 +98,10 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             if name in memory:
                 # 2. Send a 303 redirect to the long URI in memory[name].
                 #    Delete the following line.
-                raise NotImplementedError("Step 2 isn't written yet.")
+                self.send_response(303)
+                self.send_header('Location', memory[name])
+                self.end_headers()
+
             else:
                 # We don't know that name! Send a 404 error.
                 self.send_response(404)
@@ -114,25 +127,35 @@ class Shortener(http.server.BaseHTTPRequestHandler):
         # Check that the user submitted the form fields.
         if "longuri" not in params or "shortname" not in params:
             # 3. Serve a 400 error with a useful message.
-            #    Delete the following line.
-            raise NotImplementedError("Step 3 isn't written yet!")
+            self.send_response(400)
+            self.send_header('Content-type', 'text/plain; charset=utf8')
+            self.end_headers()
 
-        longuri = params["longuri"][0]
-        shortname = params["shortname"][0]
+            self.wfile.write('HTTP 400: Please submit values for both fields'.encode())
+
+        longuri = params['longuri'][0]
+        shortname = params['shortname'][0]
 
         if CheckURI(longuri):
             # This URI is good!  Remember it under the specified name.
             memory[shortname] = longuri
 
             # 4. Serve a redirect to the root page (the form).
-            #    Delete the following line.
-            raise NotImplementedError("Step 4 isn't written yet!")
+            self.send_response(303)
+            self.send_header('Location', '/')
+            self.end_headers()
+
         else:
             # Didn't successfully fetch the long URI.
 
             # 5. Send a 404 error with a useful message.
-            #    Delete the following line.
-            raise NotImplementedError("Step 5 isn't written yet!")
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain; charset=utf8')
+            self.end_headers()
+
+            self.wfile.write(
+                'HTTP 404: The original URI submitted is invalid'.encode())
+
 
 if __name__ == '__main__':
     server_address = ('', 8000)
